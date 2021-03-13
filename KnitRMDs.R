fa9e55a31e5a938ed3ed5ca51eba56c6b6607828
Allison Kunerth <akunerth@uoregon.edu>
2021-03-12 18:40:57 -0800

    Move files and render .RMDs on talapas

--- KnitRMDs.R
+++ KnitRMDs.R
@@ -1,23 +1,22 @@
 #!/usr/bin/env Rscript
 
-#chmod ug+x /projects/snaplab/akunerth/rSEALData/*
-  
-module load R
-
 if(!require(rmarkdown)) remote::install_github("rstudio/rmarkdown",
-                                             repos = "http://cran.us.r-project.org",lib= "/gpfs/home/akunerth/R/x86_64-pc-linux-gnu-library/3.6")
+                                               repos = "http://cran.us.r-project.org",lib= "/gpfs/home/akunerth/R/x86_64-pc-linux-gnu-library/3.6")
 
 library(boxr)
 #Authenticates to box with credentials
 box_auth_service()
 #Set working directory to rSEAL_Dashboard folder
 box_setwd(132210917081)
-box_dl(786237121897, overwrite = TRUE)
-test <- rmarkdown::render((box_dl(779377546442, overwrite = TRUE)))
- box_ul(
-   dir_id = 132950532816,
-   "/gpfs/projects/snaplab/akunerth/rSEALDashboard/enrollment.html",
-   pb = options()$boxr.progress,
-   description = NULL
- )                 
+#Knits/Renders file
+rmarkdown::render_site(encoding = 'UTF-8')
 
+file.remove("caseProgressionlog.csv")
+file.remove("Coach_Impressions_Form.sav")
+file.remove("Coach_Impressions_Form copy 2.sav")
+file.remove("consentTracker.csv")
+file.remove("enrollment.html")
+file.remove("enrollment.Rmd")
+file.remove("FIND_Fidelity.sav")
+file.remove("FIND_Fidelity copy 2.sav")
+file.remove("HTP_Fidelity.sav")
